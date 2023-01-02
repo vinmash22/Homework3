f47599f2c23da3c5fd7599551e06c651afb6a7bd
vinmash <vinmash@mail.ru>
2023-01-03 00:08:03 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
         byte apple = 1;
         System.out.println("переменная с типом byte " + apple);
         short days = 365;
@@ -7,11 +13,27 @@ public class Main {
         int year = 2023;
         System.out.println("переменная с типом int " + year);
         long money = 1250000L;
-        System.out.println("переменная с типом long "+ money);
+        System.out.println("переменная с типом long " + money);
         float weight = 72.5f;
         System.out.println("переменная с типом float " + weight);
         double distance = 307.358;
         System.out.println("переменная с типом double " + distance);
 
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        float variable1 = 27.12f;
+        System.out.println("переменная 1 " + variable1);
+        long variable2 = 987678965549L;
+        System.out.println("переменная 2 " + variable2);
+        double variable3 = 2.786;
+        System.out.println("переменная 3 " + variable3);
+        short variable4 = 569;
+        System.out.println("переменная 4 " + variable4);
+        int variable5 = 27897;
+        System.out.println("переменная 5 " + variable5);
+        byte variable6 = 67;
+        System.out.println("переменная 6 " + variable6);
     }
 }
