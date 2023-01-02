4f65433188dc55aed8eb5b541b8e7527c46ca3c3
vinmash <vinmash@mail.ru>
2023-01-02 23:55:25 +0500

    task1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        byte apple = 1;
+        System.out.println("переменная с типом byte " + apple);
+        short days = 365;
+        System.out.println("переменная с типом short " + days);
+        int year = 2023;
+        System.out.println("переменная с типом int " + year);
+        long money = 1250000L;
+        System.out.println("переменная с типом long "+ money);
+        float weight = 72.5f;
+        System.out.println("переменная с типом float " + weight);
+        double distance = 307.358;
+        System.out.println("переменная с типом double " + distance);
+
+    }
+}
